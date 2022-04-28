f195ab6ba53dc5da992d32bd9620bde1dc03c758
Tomas Uzpurvis <tomas605@gmail.com>
2022-04-28 21:33:00 +0300

    Serverio uzklausa ir ataskymas

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello Word")
+const express = require("express"); // sukuriam kintamaji express
+
+const app = express();
+
+
+app.listen(9000, () => {
+    console.log("Labas,serveris veikia");
+
+});
+
+app.get("/", (request, response) => {
+console.log("ivyko uzklausa");
+response.send("Hello word");
+});
+
+
+
