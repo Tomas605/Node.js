7baf78d5f7491d53c56c93ee1e623eb9f1bacabf
Tomas Uzpurvis <tomas605@gmail.com>
2022-04-30 10:34:58 +0300

    Pirma uzduotis Datos rodytas

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require("express"); // sukuriam kintamaji express
 
+const express = require('express');
 const app = express();
 
-
 app.listen(9000, () => {
-    console.log("Labas,serveris veikia");
-
+    console.log('serveris paleistas!');
 });
 
-app.get("/", (request, response) => {
-console.log("ivyko uzklausa");
-response.send("Hello word");
+app.get('/time', (request, response) => {
+    const date = new Date();
+    const dateString = date.toLocaleDateString();
+    const timeString = date.toLocaleTimeString();
+    response.send(`${dateString} ${timeString}`);
 });
-
-
 
