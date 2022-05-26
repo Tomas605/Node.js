7f61f526a5ce86b51b5530e8bb37b66b07bd4806
Tomas Uzpurvis <tomas605@gmail.com>
2022-05-26 22:15:46 +0300

    MongoDB

--- index.js
+++ index.js
@@ -2,16 +2,55 @@ require('dotenv').config();
 
 const express = require('express');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 const app = express();
 
 const knygos = [
-  'Haris Poteris',
-  'Biblija',
-  '1984',
+  'Haris porteris',
+  'Dziungliu knyga',
 ];
 
-app.listen(process.env.Port, () => {
+app.listen(process.env.PORT, () => {
   console.log('Serveris paleistas. Laukia užklausų');
+});
+
+app.get('/books', (request, response) => {
+  client.connect(async () => {
+    const database = client.db('knygu-projektas');
+    const collection = database.collection('knygos');
+    const result = await collection.find({}).toArray();
+
+    response.json(result);
+
+    client.close();
+  });
+});
+
+app.get('/books/:id', (request, response) => {
+  response.json(knygos);
+});
+
+app.post('/books', (request, response) => {
+  client.connect(async () => {
+    const database = client.db('knygu-projektas');
+    const collection = database.collection('knygos');
+    const result = await collection.insertOne({
+      name: request.body.bookName,
+      pageCount: request.body.bookPageCount,
+    });
+
+    response.json(result);
+
+    client.close();
+  });
 });
 
 app.get('/books/:from/:to', (request, response) => {
